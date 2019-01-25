b0f0dc3b73c6e8570c97537ca721e472b9e5aea9
ElectroScience <46717842+ElectroSciences@users.noreply.github.com>
2019-01-25 14:44:19 +0100

    Add files via upload

---
+++ servo_button/servo_button.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+Servo myservo;
+int pos = 0;      
+void setup() {
+  // put your setup code here, to run once:
+  myservo.attach(7);
+  pinMode(9, INPUT);
+  pinMode(7, OUTPUT);  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (digitalRead(9) == HIGH){
+  for (pos = 0; pos <= 180; pos += 1) { 
+       myservo.write(pos);
+       delay(5);      
+  }
+  for (pos = 180; pos >= 0; pos -= 1) {
+       myservo.write(pos);
+       delay(5); 
+  }
+  }
+}
